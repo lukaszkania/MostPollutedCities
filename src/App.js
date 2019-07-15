c49dc3b56c949b0cb8f545e98f86e3c8cd755561
Lukasz Kania <lukasz.kania199@gmail.com>
2019-07-15 14:30:40 +0200

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Home from './components/Home/Home';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  state = {}
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
+
